8c19ae1f3195751ad94be9696220ba11c2b6bdf8
bird <birdroid@126.com>
2012-11-13 18:35:36 +0800

    add setting

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
+/*
+ //查询用户的朋友，即相互关注
+  ！must complete setting
+ */
+var setting ={
+  email : '',      //weibo email
+  password : '',     //weibo password
+  uid : ''              //the uid of  user you intend to inquiry
+}
 var weibo = require("weiboapi")
 var EventProxy =require('eventproxy')
 var proxy = new EventProxy()
-//查询用户的朋友，即相互关注
 proxy.assign("fans", "follow", function (fans, follow) {
 	var obj = {},friends={}
     for(var i=0;i<fans.length;i++){
@@ -18,27 +26,27 @@ proxy.assign("fans", "follow", function (fans, follow) {
     debugger
 })
 
-weibo.login('email','password',function(err,API){
+weibo.login(setting.email,setting.password,function(err,API){
 	if(!err){
 		var fans = [],follow = []
-		API.getFans('uid',1,function(err,result){
-			if(err) API.getFans('uid',result.page,arguments.callee)
+		API.getFans(setting.uid,1,function(err,result){
+			if(err) API.getFans(setting.uid,result.page,arguments.callee)
 			else{
 				if( result.page < result.count){
 					fans = fans.concat(result.data)
-				    API.getFans('uid',result.page+1,arguments.callee)
+				    API.getFans(setting.uid,result.page+1,arguments.callee)
 				}else{
 					proxy.trigger("fans", fans)
 				}
 			}
 		})
-		API.getFollow('uid',1,function(err,result){
+		API.getFollow(setting.uid,1,function(err,result){
 			if(err) {
-				API.getFollow('uid',result.page,arguments.callee)
+				API.getFollow(setting.uid,result.page,arguments.callee)
 			}else{
 				if( result.page < result.count){
 					follow = follow.concat(result.data)
-				    API.getFollow('uid',result.page+1,arguments.callee)
+				    API.getFollow(setting.uid,result.page+1,arguments.callee)
 				}else{
 					proxy.trigger("follow", follow)
 				}
